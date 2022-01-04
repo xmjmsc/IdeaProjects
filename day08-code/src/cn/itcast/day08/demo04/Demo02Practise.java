4f1887a5ca2374dd34922c8208e63f6801bc6900
xmjmsc <borlow@qq.com>
2022-01-04 21:05:12 +0800

    Initial commit

--- day08-code/src/cn/itcast/day08/demo04/Demo02Practise.java
+++ day08-code/src/cn/itcast/day08/demo04/Demo02Practise.java
@@ -1,11 +1,22 @@
 package cn.itcast.day08.demo04;
 
+import java.util.Arrays;
+
 public class Demo02Practise {
     public static void main(String[] args) {
 
         String str = "duyerwbgf84873h233e2enub2138r82";
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            System.out.println(chars[i]);
+
+        }
+
+
     }
-    
+
 
 
 }
---
+++ day08-code/src/cn/itcast/day08/demo04/Demo03Math.java
@@ -0,0 +1,8 @@
+package cn.itcast.day08.demo04;
+
+public class Demo03Math {
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs(3.14));
+    }
+}
