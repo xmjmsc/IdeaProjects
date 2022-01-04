30ce38cf846a8e8a424333ac1640af44d84797ec
xmjmsc <borlow@qq.com>
2022-01-04 21:18:30 +0800

    Initial commit

--- day08-code/src/cn/itcast/day08/demo04/Demo03Math.java
+++ day08-code/src/cn/itcast/day08/demo04/Demo03Math.java
@@ -4,5 +4,7 @@ public class Demo03Math {
 
     public static void main(String[] args) {
         System.out.println(Math.abs(3.14));
+        System.out.println(Math.abs(0));
+        System.out.println(Math.abs(-2.3));
     }
 }
